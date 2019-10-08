62f903da9c1fde1608beb9ef9b5a5fdd9a5be3ae
yongmin.ko <euclidmin@gmail.com>
2019-10-08 14:52:03 +0900

    Qt designer ui 파일 추가 pyuic로 out.py 생성

--- qt_start.py
+++ qt_start.py
@@ -1,34 +1,60 @@
 import sys
 import random
 from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QWidget
+import PyQt5.QtDesigner
+from out import Ui_Form
 
-class MyWidget(QtWidgets.QWidget):
+# color_set_form_class = uic.loadUiType("color_se1.ui")[0]
+#
+#
+#
+# class MyWidget(QtWidgets.QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+#
+#         self.button = QtWidgets.QPushButton("click me!")
+#         self.text = QtWidgets.QLabel("Hello world")
+#         self.text.setAlignment(QtCore.Qt.AlignCenter)
+#
+#         self.layout = QtWidgets.QVBoxLayout()
+#         self.layout.addWidget(self.text)
+#         self.layout.addWidget(self.button)
+#         self.setLayout(self.layout)
+#
+#         self.button.clicked.connect(self.magic)
+#
+#     def magic(self):
+#         self.text.setText(random.choice(self.hello))
+
+
+
+class ColorForm(QMainWindow, Ui_Form):
     def __init__(self):
-        super().__init__()
+        super(ColorForm, self).__init__()
+        self.setupUi(self)
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("click me!")
-        self.text = QtWidgets.QLabel("Hello world")
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
-
-        self.button.clicked.connect(self.magic)
-
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
 
 
 if __name__=="__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    # widget = MyWidget()
+    # widget.resize(800, 600)
+    # widget.show()
 
+    main_window = QWidget()
+    ui = Ui_Form()
+    # ui = color_set_form_class
+    ui.setupUi(main_window)
+    main_window.show()
+    #
+    # color_form = ColorForm()
+    # # color_form.__init__()
+    # # color_form.resize(800, 800)
+    # color_form.show()
     sys.exit(app.exec_())
 
